4e78e01e30fa9ebbcecfe7d4cf2dea8f375d62df
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2022-11-23 13:32:09 +0800

    數字動畫
    數字動畫

--- js/self_index.js
+++ js/self_index.js
@@ -38,3 +38,36 @@ $('#ham_btn').click(()=>{
 })
 
 AOS.init();
+
+
+// Alex data_box
+var data_items = $('#data_box > li');
+var runTime = 750;//基礎值0.75秒
+var minCount = 8; //最少跑次數
+for (let i = 0; i < data_items.length; i++) {
+    const element = data_items[i];
+	runNumHandler(i,element.dataset.txt.replace(/,/gi,''));
+}
+function runNumHandler(item,txt) {
+	var sec = runTime/minCount;
+	var startCount = 0;
+	var rangeCount = parseInt(txt / minCount);
+	var runNum = setInterval(()=>{	
+		if(startCount<txt){
+			startCount=startCount+rangeCount
+			if(startCount>txt){
+				startCount=txt;
+			}
+			data_items[item].dataset.txt=startCount;
+		}else{
+			let itemTxt = data_items[item].dataset.txt;
+			if(itemTxt >999){
+				itemTxt = parseInt(itemTxt).toFixed(2).replace(/(\d{1,3})(?=(\d{3})+(?:$|\.))/g,'$1,')
+				itemTxt = itemTxt.split('.')
+				data_items[item].dataset.txt=itemTxt[0];
+			}
+			clearInterval(runNum);
+		}     
+    }, sec);
+}
+// Alex data_box
